e4ba1f78ac192366b94b01f458c14e9b8fa04e45
AmithBaliza <137040896+AmithBaliza@users.noreply.github.com>
2023-08-04 10:50:17 +0530

    CHORE: Added base lib files

---
+++ lib/services/base.d.ts
@@ -0,0 +1,11 @@
+/**
+ * Base global class that contains globally
+ * needed methods and variables
+ */
+declare class BaseService {
+
+  // Add necessary global functions and variables here
+
+}
+
+export default BaseService;
